f32fa0b9ca6f5d8d449b0020137123dd3c18ee3c
yamada <zhu4bi1@hotmail.com>
2019-03-13 18:21:29 +0800

    complete quiz3

--- quiz3.py
+++ quiz3.py
@@ -1,7 +1,19 @@
+import sys
+
 def collatz(number):
- try:
-  type#判断类型
- except
-  print('Error:Invalid argument.')
-  
- number = 5
+  if False == isinstance(number,int) :     #判断类型
+    print('Error:Invalid argument.')
+    sys.exit()
+
+  if 0 == number%2:
+    number = number/2
+  else:
+    number = 3*number+1
+  print(int(number))
+  return number
+
+
+value=int(input('input value for int:'))
+while 1 != value:
+  value = int(collatz(value))
+print('Program End.')
