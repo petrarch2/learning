90054cfcf35c46e92457df4cb86b7e7c461e68df
yamada <zhu4bi1@hotmail.com>
2019-03-14 01:04:30 +0800

    tab
    tab = 4 space

--- Source/quiz3.py
+++ Source/quiz3.py
@@ -1,19 +1,21 @@
 import sys
 
 def collatz(number):
-  if False == isinstance(number,int) :     #判断类型
-    print('Error:Invalid argument.')
-    sys.exit()
+    if False == isinstance(number,int) :     #判断类型
+        print('Error:Invalid argument.')
+        sys.exit()
 
-  if 0 == number%2:
-    number = number/2
-  else:
-    number = 3*number+1
-  print(int(number))
-  return number
+    if 0 == number%2:
+        number = number/2
+    else:
+        number = 3*number+1
+
+    print(int(number))
+    return number
 
 
 value=int(input('input value for int:'))
+
 while 1 != value:
-  value = int(collatz(value))
+    value = int(collatz(value))
 print('Program End.')
