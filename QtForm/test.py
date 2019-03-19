122ada652236544f3e75f80a4164debf29dab495
NERO <zhu4bi1@hotmail.com>
2019-03-19 20:01:12 +0800

    test PyQt5-tools

---
+++ QtForm/test.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'test.ui'
+#
+# Created by: PyQt5 UI code generator 5.12
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(22)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.timeEdit = QtWidgets.QTimeEdit(self.centralwidget)
+        self.timeEdit.setObjectName("timeEdit")
+        self.horizontalLayout.addWidget(self.timeEdit)
+        self.dial = QtWidgets.QDial(self.centralwidget)
+        self.dial.setObjectName("dial")
+        self.horizontalLayout.addWidget(self.dial)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 1, 1, 1)
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.frame)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.label_2 = QtWidgets.QLabel(self.frame)
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout_2.addWidget(self.label_2)
+        self.lineEdit = QtWidgets.QLineEdit(self.frame)
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout_2.addWidget(self.lineEdit)
+        self.horizontalLayout_2.addLayout(self.verticalLayout_2)
+        self.pushButton_2 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout_2.addWidget(self.pushButton_2)
+        self.gridLayout.addWidget(self.frame, 1, 1, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "This is a Test"))
+        self.pushButton.setText(_translate("MainWindow", "按我！"))
+        self.label_2.setText(_translate("MainWindow", "This is a Frame of Containers"))
+        self.pushButton_2.setText(_translate("MainWindow", "Push!"))
+
+
--- Source/url_test.py
+++ Source/url_test.py
@@ -6,8 +6,35 @@ __date__ = '2019/3/18'
 
 import configparser,os
 import urllib.request,urllib.parse,socket,urllib.error
-import requests
+import requests,json,re
 
+'''
+正则表达式常用的匹配模式
+\w      匹配字母数字及下划线
+\W      匹配f非字母数字下划线
+\s      匹配任意空白字符，等价于[\t\n\r\f]
+\S      匹配任意非空字符
+\d      匹配任意数字
+\D      匹配任意非数字
+\A      匹配字符串开始
+\Z      匹配字符串结束，如果存在换行，只匹配换行前的结束字符串
+\z      匹配字符串结束
+\G      匹配最后匹配完成的位置
+\n      匹配一个换行符
+\t      匹配一个制表符
+^       匹配字符串的开头
+$       匹配字符串的末尾
+.       匹配任意字符，除了换行符，re.DOTALL标记被指定时，则可以匹配包括换行符的任意字符
+[....]  用来表示一组字符，单独列出：[amk]匹配a,m或k
+[^...]  不在[]中的字符：[^abc]匹配除了a,b,c之外的字符
+*       匹配0个或多个的表达式
++       匹配1个或者多个的表达式
+?       匹配0个或1个由前面的正则表达式定义的片段，非贪婪方式
+{n}     精确匹配n前面的表示
+{m,m}   匹配n到m次由前面的正则表达式定义片段，贪婪模式
+a|b     匹配a或者b
+()      匹配括号内的表达式，也表示一个组
+'''
 # base = os.path.splitdrive(os.path.realpath(__file__))[0]
 # base = os.path.realpath(__file__).split('\\')
 # conf_path = os.path.join(base[0]+'\\',base[1],"conf.ini")
@@ -61,7 +88,15 @@ if 0:
     response = opener.open('https://bilibili.com')
 
 else:
-    response = requests.get(url)
+    url = 'https://nhentai.net/g/209901/'
+    headers = {'User-Agent': 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X)\
+        AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25'}
+    response = requests.get(url, headers=headers)
     print(response.encoding)
-    print(response.content.decode("utf-8"))
+    print(response.status_code)
+    #print(response.content.decode("utf-8"))
+    content = response.text
+    result = re.findall('^g.209901$',content)#result = re.match('^.\w.\d{6}.\d{3}.$', content)
+    print(result)
+    print(result.group())
 
