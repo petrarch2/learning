458e67f466f1b8ad7443ad7d9e03b8ea228991a1
NERO <zhu4bi1@hotmail.com>
2019-03-20 17:31:23 +0800

    learn how to use swig and distutils to add c/c++ files in a python project(and you can use cython to change python files to c)

---
+++ Source/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+__author__ = 'NERO'
+__date__ = '2019/3/20'
+
+"""
+    setup.py file for SWIG example
+"""
+from distutils.core import setup, Extension
+
+example_module = Extension('_example',
+                               sources=['example_wrap.c', 'example.c'],
+                               )
+setup(name = 'example',
+    version = '0.1',
+    author = "SWIG Docs",
+    description = """Simple swig example from docs""",
+    ext_modules = [example_module],
+    py_modules = ["example"],
+    )
--- Source/url_test.py
+++ Source/url_test.py
@@ -7,7 +7,7 @@ __date__ = '2019/3/18'
 import configparser,os
 import urllib.request,urllib.parse,socket,urllib.error
 import requests,json,re
-
+#python $FileName$ build_ext --inplace
 '''
 正则表达式常用的匹配模式
 \w      匹配字母数字及下划线
@@ -45,6 +45,7 @@ conf_path = os.path.join(base,"conf.ini")
 config  = configparser.ConfigParser()
 config.read(conf_path)
 url = config['DEFAULT']['url']
+keyword = config['DEFAULT']['keyword']
 if 0:
     #post方式请求
     data = bytes(urllib.parse.urlencode({'word': 'hello'}), encoding='utf8')    #将字典转换为url参数
@@ -88,15 +89,18 @@ if 0:
     response = opener.open('https://bilibili.com')
 
 else:
-    url = 'https://nhentai.net/g/209901/'
     headers = {'User-Agent': 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X)\
         AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25'}
     response = requests.get(url, headers=headers)
     print(response.encoding)
     print(response.status_code)
-    #print(response.content.decode("utf-8"))
+    # print(response.content.decode("utf-8"))
     content = response.text
-    result = re.findall('^g.209901$',content)#result = re.match('^.\w.\d{6}.\d{3}.$', content)
-    print(result)
-    print(result.group())
+    pattern = re.compile(keyword, re.S)     #将正则表达式编译成正则表达式对象，方便复用该正则表达式
+    results = re.findall(pattern, content)#result = re.match('^.\w.\d{6}.\d{3}.$', content)
+    print(len(results))
+
+    for result in results:
+        url, name, author, date = result
+        print(url, name, author, date)
 
